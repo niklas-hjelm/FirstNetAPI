831f4c26e64df4367a51258cc5380c0795bad859
NiklasHj <54146045+NiklasHj@users.noreply.github.com>
2022-02-20 22:16:17 +0100

    fix

--- FirstNetAPI/Program.cs
+++ FirstNetAPI/Program.cs
@@ -5,37 +5,35 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<AnimalSqlRepository>();
-builder.Services.AddSingleton<AnimalDocRepository>();
-
 var app = builder.Build();
 
 app.UseStaticFiles();
 
 app.MapGet("/", () => Results.Redirect("index.html"));
 
-app.MapGet("/animals", ([FromServices] AnimalDocRepository repo) => repo.GetAll());
+app.MapGet("/animals", ([FromServices] AnimalSqlRepository repo) => repo.GetAll());
 
-app.MapGet("/animals/{id}", ([FromServices] AnimalDocRepository repo, string id) =>
+app.MapGet("/animals/{id}", ([FromServices] AnimalSqlRepository repo, int id) =>
 {
     var animal = repo.GetById(id);
 
     return Results.Ok(animal);
 });
 
-app.MapPost("/animals", ([FromServices] AnimalDocRepository repo, AnimalDoc animal) =>
+app.MapPost("/animals", ([FromServices] AnimalSqlRepository repo, AnimalRow animal) =>
 {
     repo.Create(animal);
     var animals = repo.GetAll();
     return Results.Created($"animals/{animal.Id}",animals);
 });
 
-app.MapPut("/animals", ([FromServices] AnimalDocRepository repo, AnimalDoc animal) =>
+app.MapPut("/animals", ([FromServices] AnimalSqlRepository repo, AnimalRow animal) =>
 {
     repo.Update(animal);
     return Results.Ok();
 });
 
-app.MapDelete("/animals/{id}", ([FromServices] AnimalDocRepository repo, string id) =>
+app.MapDelete("/animals/{id}", ([FromServices] AnimalSqlRepository repo, int id) =>
 {
     repo.Delete(id);
     var animals = repo.GetAll();
